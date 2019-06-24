ef844cdecbdce302fdbe20aa4ed5eeafaf32b811
Julio A. Peraza-Goicolea <52050407+JulioAPeraza@users.noreply.github.com>
2019-06-24 16:49:48 -0400

    Delete susceptibility.m

--- fucntions/susceptibility.m
+++
@@ -1,9 +0,0 @@
-function chi = susceptibility(B, N, d, m, v_ijk, chi, jIdx)
-
-    for k=1:N
-        sum_v = accumarray(jIdx,v_ijk(:,k),[],@sum);
-        chi(:,k) = (sum_v).*(1-m.^2);
-        chi(k,k) = 0;% diagonal values of the matrix formed by elements chi_ij are set to 0)
-    end
-    
-end
